e101a4e3afa3404306537cfde004acc3223badb9
Felix Bade <felix@bade.fi>
2014-02-16 17:08:00 +0200

    Added Youtube info

--- link_handler.py
+++ link_handler.py
@@ -9,45 +9,22 @@ from url import getLastURL
 
 YOUTUBE_BANNER = '\00314[\00300You\00304Tube\00314]\x0f'
 
-# TODO: non-ascii? newlines?
+# TODO: non-ascii? newlines in title?
 def link_handler(message, bot):
-	url = getLastURL(message)
-	url = getFinalRedirect(url)
-
-	if isYoutubeLink(url):
-		info = getYoutubeVideoInfo(url)
-
-	else:
-		info = getLinkInfo(url)
-	
-	bot.reply(info)
+	url = getFinalRedirect(getLastURL(message))
+	bot.reply(getInfoAboutURL(url))
 
 def getFinalRedirect(url):
 	return urllib2.urlopen(url).geturl()
 
+def getInfoAboutURL(url):
+	if isYoutubeLink(url):
+		return getYoutubeVideoInfo(url)
+	else:
+		return getLinkInfo(url)
+
 def isYoutubeLink(url):
-	return False # youtube info is not yet implemented
-	#return '://www.youtube.com/watch' in url
-
-def getYoutubeVideoInfo(url):
-	youtube_page = getYoutubePageSoup(url)
-	title = getYoutubeVideoTitle(youtube_page)
-	duration = getYoutubeVideoDuration(youtube_page)
-	uploader = getYoutubeVideoUploader(youtube_page)
-	banner = YOUTUBE_BANNER
-	return '%s \002%s\002 (%s) by %s' % (banner, title, duration, uploader)
-
-def getYoutubePageSoup(url):
-	return BeautifulSoup(urllib2.urlopen(url).read())
-
-def getYoutubeVideoTitle(youtube_page):
-	return 'Title'
-
-def getYoutubeVideoDuration(youtube_page):
-	return '0:00'
-
-def getYoutubeVideoUploader(youtube_page):
-	return 'nobody'
+	return '://www.youtube.com/watch' in url
 
 def getLinkInfo(url):
 	try:
@@ -55,9 +32,58 @@ def getLinkInfo(url):
 		if isHTML(raw_http):
 			return getTitleMessage(raw_http)
 		else:
-			return getFileInfoMessage(raw_http)
+			return getFileInfo(raw_http)
 	except urllib2.HTTPError as e:
 		return getErrorMessage(e)
+
+
+
+def getYoutubeVideoInfo(url):
+	youtube_page = getYoutubePageSoup(url)
+	title = getYoutubeVideoTitle(youtube_page)
+	duration = getYoutubeVideoDuration(youtube_page)
+	uploader = getYoutubeVideoUploader(youtube_page)
+	banner = YOUTUBE_BANNER
+	return '%s \x02%s\x0f (%s) by %s' % (banner, title, duration, uploader)
+
+def getYoutubePageSoup(url):
+	return BeautifulSoup(urllib2.urlopen(url).read())
+
+def getYoutubeVideoTitle(youtube_page):
+	for meta_tag in youtube_page.find_all('meta'):
+		if 'name' in meta_tag.attrs and meta_tag.attrs['name'] == 'title':
+			title = meta_tag.attrs['content']
+			return title
+	return '???'
+
+def getYoutubeVideoDuration(youtube_page):
+	for meta_tag in youtube_page.find_all('meta'):
+		if 'itemprop' in meta_tag.attrs and meta_tag.attrs['itemprop'] == 'duration':
+			return getDurationFromYoutubeString(meta_tag.attrs['content'])
+	return '?:??'
+
+def getYoutubeVideoUploader(youtube_page):
+	tags = youtube_page.find_all("span", class_='yt-user-name')
+	if len(tags) > 0:
+		return tags[0].text
+	else:
+		return '???'
+
+def getDurationFromYoutubeString(length):
+	# example: PT10M44S = 10 minutes, 44 seconds
+	minutes = int(length[2 : length.find('M')])
+	seconds = int(length[length.find('M')+1 : length.find('S')])
+	return getDurationStringFromMinutesAndSeconds(minutes, seconds)
+
+def getDurationStringFromMinutesAndSeconds(minutes, seconds):
+	if minutes < 60:
+		return '%i:%02i' % (minutes, seconds)
+	else:
+		hours = minutes / 60
+		minutes = minutes % 60
+		return '%i:%02i:%02i' % (hours, minutes, seconds)
+
+
 
 def isHTML(raw_http):
 	content_type = raw_http.info().getheaders('Content-Type')[0]
@@ -73,17 +99,21 @@ def getTitleMessage(raw_http):
 	else:
 		return 'No title'
 
-def getFileInfoMessage(raw_http):
-	length = getContentLengthInKB(raw_http)
+def getFileInfo(raw_http):
+	length = getContentLength(raw_http)
 	file_type = getFileType(raw_http)
-	return '%s kB, type: %s' % (length, file_type)
+	return '%s, type: %s' % (length, file_type)
 
-def getContentLengthInKB(raw_http):
-	if len(raw_http.info().getheaders('Content-Length')) > 0:
-		length_in_bytes = int(raw_http.info().getheaders('Content-Length')[0])
-		return str(round(length_in_bytes/1024.0,1))
-	else:
-		return '???'
+def getContentLength(raw_http):
+	try:
+		length = dummyGetContentLengthInKB(raw_http)
+	except:
+		length = '???'
+	return '%s kB' % length
+
+def dummyGetContentLengthInKB(raw_http):
+	length_in_bytes = int(raw_http.info().getheaders('Content-Length')[0])
+	return str(round(length_in_bytes/1024.0, 1))
 
 def getFileType(raw_http):
 	types = raw_http.info().getheaders('Content-Type')
