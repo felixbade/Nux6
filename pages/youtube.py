796c5c38f15dbf3c5793570956e46d7e630b557c
Felix Bade <felix@bade.fi>
2014-07-16 20:05:16 +0300

    Minor bugs

--- pages/youtube.py
+++ pages/youtube.py
@@ -14,7 +14,7 @@ class YouTube(Page):
 			return None
 		duration = self.getDuration()
 		author = self.getAuthor() or 'an unknown user'
-		return '\x03%s\x0f (%s) by %s' % (title, duration, author)
+		return '\x02%s\x0f (%s) by %s' % (title, duration, author)
 
 	def getTitle(self):
 		return self.getMetaInformation('name', 'title')
