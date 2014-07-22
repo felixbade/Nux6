15402c29771f020a2aaa0966b5bd388b3897eed3
Felix Bade <felix@bade.fi>
2014-07-22 13:01:07 +0300

    Un hardcoded irc server

--- main.py
+++ main.py
@@ -8,8 +8,20 @@ from twisted.python import log
 
 from factory import IRCBotFactory
 
+if len(sys.argv) < 2:
+    print 'An IRC bot.'
+    print 'Usage: %s <host> [<port>]' % sys.argv[0]
+    exit(1)
+
+host = sys.argv[1]
+port = 6667
+try:
+    port = int(sys.argv[2])
+except IndexError, ValueError:
+    pass
+
 if __name__ == '__main__':
 	log.startLogging(sys.stdout)
 	factory = IRCBotFactory()
-	reactor.connectTCP('irc.zah.fi', 6667, factory)
+	reactor.connectTCP(host, port, factory)
 	reactor.run()
