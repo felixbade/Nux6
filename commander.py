d7d28c2a4152c8e75e98cc4ce6f4be5714f475a9
Felix Bade <felix@bade.fi>
2014-09-08 17:33:14 +0300

    Increase minimum url length for automatic shorten

--- commander.py
+++ commander.py
@@ -27,6 +27,6 @@ class Commander:
 
 		if hasURL(message):
 			url = getLastURL(message)
-			if len(url) > 30 and nocmd:
-				self.bot.reply(shorten(url))
 			self.bot.reply(URLInfo(url).getInfo())
+			if len(url) > 50 and nocmd:
+				self.bot.reply(shorten(url))
