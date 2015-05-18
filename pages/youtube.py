2087113e2e83a3bcf3ae8686ec6295323c08cdc5
Felix Bade <felix@bade.fi>
2015-05-18 12:26:40 +0300

    Update youtube link info

--- pages/youtube.py
+++ pages/youtube.py
@@ -14,7 +14,8 @@ class YouTube(Page):
 			return None
 		duration = self.getDuration()
 		author = self.getAuthor() or 'an unknown user'
-		return '\x02%s\x0f (%s) by %s' % (title, duration, author)
+		#return '\x02%s\x0f (%s) by %s' % (title, duration, author)
+		return '%s (%s) by %s' % (title, duration, author)
 
 	def getTitle(self):
 		return self.getMetaInformation('name', 'title')
@@ -28,8 +29,9 @@ class YouTube(Page):
 		return minutes * 60 + seconds
 
 	def getAuthor(self):
-		tags = self.soup.find_all('span', class_='yt-user-name')
+		#tags = self.soup.find_all('span', class_='yt-user-name')
+		tags = self.soup.find_all(class_='yt-user-info')
 		if len(tags) > 0:
-			return tags[0].text
+			return tags[0].text.replace('\n', '')
 		else:
 			return None
